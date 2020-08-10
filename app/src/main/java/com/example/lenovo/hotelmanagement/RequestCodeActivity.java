a50b1f5cae5125201eb6970340f7795ca823aab0
Produde96 <35090273+Produde96@users.noreply.github.com>
2020-08-10 13:56:47 +0530

    Request Code Activity is commited

---
+++ app/src/main/java/com/example/lenovo/hotelmanagement/RequestCodeActivity.java
@@ -0,0 +1,39 @@
+package com.example.lenovo.hotelmanagement;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class RequestCodeActivity extends AppCompatActivity {
+
+    private EditText editTextMobile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_code);
+
+        editTextMobile = findViewById(R.id.editTextMobile);
+
+        findViewById(R.id.buttonContinue).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String mobile = editTextMobile.getText().toString().trim();
+
+                if(mobile.isEmpty() || mobile.length() < 10){
+                    editTextMobile.setError("Enter a valid mobile");
+                    editTextMobile.requestFocus();
+                    return;
+                }
+
+                Intent intent = new Intent(RequestCodeActivity.this, SendVerificatioCodeActivity.class);
+                intent.putExtra("mobile", mobile);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
